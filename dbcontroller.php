1eacd8598fb8e13629df72342a06d634f96ffa2c
Milan Nellestijn <323101@student.mboutrecht.nl>
2020-03-18 10:28:40 +0100

    Update dbcontroller.php

--- dbcontroller.php
+++ dbcontroller.php
@@ -3,7 +3,7 @@ class DBController {
 	private $host = "localhost";
 	private $user = "root";
 	private $password = "";
-	private $database = "ewatchshop";
+	private $database = "";
 	private $conn;
 
 	function __construct() {
